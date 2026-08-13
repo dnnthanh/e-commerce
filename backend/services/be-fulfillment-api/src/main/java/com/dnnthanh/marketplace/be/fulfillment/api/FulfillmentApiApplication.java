7039e517321a6d09b1daaada0d2c2a4fffe81529
Đặng Ngọc Thanh <113039832+dnnthanh@users.noreply.github.com>
2026-08-13 23:55:13 +0700

    fix(fulfillment): provide application clock

--- backend/services/be-fulfillment-api/src/main/java/com/dnnthanh/marketplace/be/fulfillment/api/FulfillmentApiApplication.java
+++ backend/services/be-fulfillment-api/src/main/java/com/dnnthanh/marketplace/be/fulfillment/api/FulfillmentApiApplication.java
@@ -1,12 +1,19 @@
 package com.dnnthanh.marketplace.be.fulfillment.api;
 
+import java.time.Clock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "com.dnnthanh.marketplace.be")
 @EnableScheduling
 public class FulfillmentApiApplication {
+
+    @Bean
+    Clock clock() {
+        return Clock.systemUTC();
+    }
 
     /**
      * Starts be-fulfillment-api.
